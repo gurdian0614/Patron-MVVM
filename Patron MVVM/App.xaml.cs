3a5c33658f0b6b476fbda005ea447a295b127e68
gurdian0614 <ivan.gurdian@outlook.com>
2023-12-09 00:47:09 -0600

    Add project files.

---
+++ Patron MVVM/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Patron_MVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Patron MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Patron_MVVM.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private int count;
+
+        // La anotacion agrega internamente SumarCommand
+        [RelayCommand]
+        public void Sumar()
+        {
+            Count++;
+        }
+    }
+}
